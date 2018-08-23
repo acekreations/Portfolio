a1b9333d92c450f6b3abb8189825c74a83103880
Craig Melville <acekreations@gmail.com>
2018-08-22 22:23:04 -0700

    add html structure and styling

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  //Initialize the bootstrap tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Scrollreveal
+  ScrollReveal().reveal('.project');
+
+  //Bounce the down arrow every 3 seconds
+  var bounceArrow = setInterval(function(){
+    $("#down-arrow").toggleClass("bounce");
+  }, 1000 * 3);
+
+  // Clear bounce interval when the user scrolls
+  $(window).scroll(function() {
+    clearInterval(bounceArrow);
+  });
+
+});
