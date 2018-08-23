e62087cbb876024b545a7f7066cdb4a730706b7a
Craig Melville <acekreations@gmail.com>
2018-08-22 22:34:38 -0700

    add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ $(function(){
 
   //Bounce the down arrow every 3 seconds
   var bounceArrow = setInterval(function(){
+    $("#down-arrow").removeClass("fadeInUp faster");
     $("#down-arrow").toggleClass("bounce");
   }, 1000 * 3);
 
