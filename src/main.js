0721c6b4e7e02cad06b663d574f3788ecc4bedb3
Craig Melville <acekreations@gmail.com>
2019-03-07 12:56:55 -0800

    add final copy and code clean up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+//Portfolio section logic
 const portfolioBtn = document.getElementById("portfolioBtn");
-// const portfolioMenu = document.getElementById("portfolioMenu");
 const portfolioContainer = document.getElementById("portfolioContainer");
 const projectInfo = document.getElementById("projectInfo");
 const portfolioMenuItems = portfolioMenu.querySelectorAll("li");
@@ -7,21 +7,17 @@ const portfolioArrowForward = document.getElementById("portfolioArrowForward");
 const portfolioArrowBackward = document.getElementById(
     "portfolioArrowBackward"
 );
-let portfolioOpen;
 let currentProjectKey = 0;
 
 portfolioBtn.addEventListener("click", function(e) {
     e.preventDefault();
 
-    if (portfolioOpen) {
-        closePortfolio();
-    } else {
-        portfolioContainer.style.display = "flex";
-        portfolioOpen = true;
+    closeAllContainers();
 
-        //load first project
-        getProjectData(portfolioMenuItems[0]);
-    }
+    portfolioContainer.style.display = "flex";
+
+    //load first project
+    getProjectData(portfolioMenuItems[0]);
 });
 
 portfolioMenuItems.forEach(function(elem, key) {
@@ -90,101 +86,31 @@ function selectedProjectUnderline(clickeElem) {
     clickeElem.classList.add("selected");
 }
 
-// portfolioBtn.addEventListener("click", function(e) {
-//     e.preventDefault();
+//About section logic
+const aboutBtn = document.getElementById("aboutBtn");
+const aboutContainer = document.getElementById("aboutContainer");
 
-//     if (portfolioOpen) {
-//         closePortfolio();
-//     } else {
-//         //remove menu container animation classes
-//         portfolioMenu.style.display = "flex";
-//         portfolioMenu.classList.remove("fadeOut");
-//         portfolioMenu.classList.add("fadeIn");
+aboutBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    closeAllContainers();
 
-//         ////remove menu item animation classes
-//         for (i = 0; i < portfolioLI.length; i++) {
-//             portfolioLI[i].classList.remove("animated", "zoomOut");
-//         }
+    aboutContainer.style.display = "flex";
+});
 
-//         //create and shuffle array of random nums
-//         let shuffleArr = [];
-//         for (i = 0; i < portfolioLI.length; i++) shuffleArr.push(i);
-//         shuffleArr = shuffle(shuffleArr);
-//         let count = 0;
-//         //animate each menu item at a random time
-//         function zoomIn() {
-//             if (count >= portfolioLI.length) {
-//                 return;
-//             }
+//Contact section logic
+const contactBtn = document.getElementById("contactBtn");
+const contactContainer = document.getElementById("contactContainer");
 
-//             portfolioLI[shuffleArr[count]].classList.add(
-//                 "animated",
-//                 "zoomIn",
-//                 "fast"
-//             );
-//             portfolioLI[shuffleArr[count]].style.opacity = "1";
+contactBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    closeAllContainers();
 
-//             count++;
-//             const interval = Math.random() * (100 - 25) + 25;
-//             setTimeout(zoomIn, interval);
-//         }
-//         zoomIn();
+    contactContainer.style.display = "flex";
+});
 
-//         portfolioOpen = true;
-//     }
-// });
-
-//handle portfolio display for each li element
-// portfolioLI.forEach(function(elem) {
-//     elem.addEventListener("click", function() {
-//         const name = this.dataset.name;
-//         showPortfolioDisplay(name);
-//     });
-// });
-
-// function showPortfolioDisplay(name) {
-//     const portfolioDisplay = document.getElementById("portfolioDisplay");
-
-//     function updateText(name) {
-//         portfolioDisplay.querySelector("#name").textContent = name;
-//     }
-
-//     if (!portfolioDisplayOpen) {
-//         portfolioDisplay.style.display = "block";
-//         portfolioDisplay.classList.add("animated", "fadeInRightBig");
-//         portfolioDisplayOpen = true;
-//         updateText(name);
-//     } else {
-//         updateText(name);
-//     }
-// }
-
-function closePortfolio() {
-    //toggle menu container animation classes
-    // portfolioMenu.classList.remove("zoomIn");
-    // portfolioMenu.classList.add("fadeOut");
-
+//Site wide logic
+function closeAllContainers() {
+    aboutContainer.style.display = "none";
     portfolioContainer.style.display = "none";
-    // portfolioDisplay.classList.remove("animated", "fadeInRightBig");
-
-    //toggle menu item animation classes
-    // for (i = 0; i < portfolioLI.length; i++) {
-    //     portfolioLI[i].classList.remove("animated", "zoomIn", "fast");
-    //     portfolioLI[i].classList.add("animated", "zoomOut");
-    //     portfolioLI[i].style.opacity = "0";
-    // }
-
-    portfolioOpen = false;
-    // portfolioDisplayOpen = false;
-}
-
-function shuffle(arr) {
-    var j, x, i;
-    for (i = arr.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = arr[i];
-        arr[i] = arr[j];
-        arr[j] = x;
-    }
-    return arr;
+    contactContainer.style.display = "none";
 }
