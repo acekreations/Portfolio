4dac2fffe947b0a1fe05e93ef2f3b84ced6e6c34
Craig Melville <acekreations@gmail.com>
2019-08-18 00:45:25 -0700

    add kanban

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -2,7 +2,10 @@
   <div>
     <div v-for="(project, index) in data" v-bind:key="index">
       <h2>
-        <router-link :to="{path: '/project/' + index}">{{ project.name }} - {{ project.tagline }}</router-link>
+        <router-link :to="{path: '/project/' + index}">
+          {{ project.name }} -
+          {{ project.tagline }}
+        </router-link>
       </h2>
       <h4>{{ project.tech }}</h4>
     </div>
