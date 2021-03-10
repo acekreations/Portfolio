c6430b27f7cbb61b561891cde8a26258ae0c7fdb
Craig Melville <acekreations@gmail.com>
2021-03-10 13:16:39 -0800

    reverting to old design

---
+++ README.md
@@ -0,0 +1,31 @@
+# Portfolio
+
+[Live Site](https://www.craigsportfolio.com/)
+
+## Overview
+
+Creating a website that represents myself and my work has always been a challenging task for me, mainly around the design. I find when designing something for myself I want to dart off in every direction, every style and every design trend I can think of. When I set out to design this new iteration of my portfolio I decided I wanted to make it space themed. Using a space theme I was able to hit the design ideas I wanted most which was bright colors, playfulness, freedom to be interpretive about the design, room to do complex animations without them feeling forced, and something that was more than just a boilerplate portfolio.
+
+## Tech
+
+-   HTML
+-   Sass
+-   Javascript
+-   [Chroma JS](https://github.com/gka/chroma.js/)
+-   [animate.css](https://github.com/daneden/animate.css)
+-   [hover.css](http://ianlunn.github.io/Hover/)
+-   [Font Awesome](https://fontawesome.com/)
+
+## Challenges
+
+The main challenge I faced here was actually not coding related but animation related. As I'm not an animator nor did I have any sort of animated prototypes to work off of I spent hours tweaking the animations of the starry background and the meteors trying to make them look just right. In the future, I would like to explore some animation tools or prototyping tools that would allow me to play around with the feel of an animation in some sort of GUI before for I even start coding.
+
+## Learning Points
+
+Form vs. Function
+
+I've worked with HTML canvas before, utilizing an exspensive animation and I knew that I could very well be facing performance issues this time around as well. Although I never faced significant performance issues on this project I did learn that even when your not taxing the client it's still worth exploring more resource sensitive options. I say that because in the first iteration of this site I was generating and rotating the _starry background_ via canvas, it looked great and was still viable performance wise, but I wondered to myself if I could do it in a way that gave a similar result but was still more performant(and ultimately much simpler). What I did was generated an svg that looked just like the stars I was generating with canvas, put that in a div and used css animation to rotate the div. Voila! I had a nearly identicle look to the previous iteration but I was saving on resources and reducing dozens of lines of code. Now I'll admit that by doing it this way I lost a lot of control over the look and feel of the starry background, meaning I could not just change a few parameters to change the look of the stars or their locations, I would need to regenerate a new svg to do that but what I learned is that a little trickery can give you a result very close to what you desire with much simple implementation minus a bit of control. I liken this to practical effects vs CG effects in movies, sure you could blow up 6 cars to get the hero shot at the end of car chase but you could get almost the same exact effect with computer generated effects for significantly less resources.
+
+## Author
+
+[Craig Melville](https://www.craigsportfolio.com/)
---
+++ js/main.js
@@ -0,0 +1,123 @@
+//Portfolio section logic
+const portfolioBtn = document.getElementById("portfolioBtn");
+const portfolioContainer = document.getElementById("portfolioContainer");
+const projectInfo = document.getElementById("projectInfo");
+const portfolioMenuItems = portfolioMenu.querySelectorAll("li");
+const portfolioArrowForward = document.getElementById("portfolioArrowForward");
+const portfolioArrowBackward = document.getElementById(
+    "portfolioArrowBackward"
+);
+var currentProjectKey = 0;
+
+//remove portfolio animation meant only for when landing on site
+setTimeout(function() {
+    portfolioContainer.classList.remove("animated", "fadeIn", "delay-1s");
+}, 2000);
+
+portfolioBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    closeAllContainers();
+
+    portfolioContainer.style.display = "flex";
+
+    //load first project
+    getProjectData(portfolioMenuItems[0]);
+});
+
+for (var key = 0; key < portfolioMenuItems.length; key++) {
+    const elem = portfolioMenuItems[key];
+    elem.dataset.key = key;
+    elem.addEventListener("click", function() {
+        getProjectData(this);
+    });
+}
+
+portfolioArrowForward.addEventListener("click", function() {
+    if (currentProjectKey < portfolioMenuItems.length - 1) {
+        const key = parseInt(currentProjectKey) + 1;
+        getProjectData(portfolioMenuItems[key]);
+    } else {
+        getProjectData(portfolioMenuItems[0]);
+    }
+});
+portfolioArrowBackward.addEventListener("click", function() {
+    if (currentProjectKey > 0) {
+        const key = parseInt(currentProjectKey) - 1;
+        getProjectData(portfolioMenuItems[key]);
+    } else {
+        getProjectData(portfolioMenuItems[portfolioMenuItems.length - 1]);
+    }
+});
+
+function getProjectData(elem) {
+    const name = elem.dataset.name;
+    const github = elem.dataset.github;
+    const link = elem.dataset.link;
+    const role = elem.dataset.role;
+    const tech = elem.dataset.tech;
+    const description = elem.dataset.description;
+    const image = elem.dataset.image;
+    displayProject(name, github, link, role, tech, description, image);
+    selectedProjectUnderline(elem);
+
+    //set project key
+    const key = elem.dataset.key;
+    currentProjectKey = key;
+}
+
+function displayProject(name, github, link, role, tech, description, image) {
+    document.getElementById("projectName").textContent = name;
+    document.getElementById("projectLink").href = link;
+    document.getElementById("projectRole").textContent = role;
+    document.getElementById("projectTech").textContent = tech;
+    document.getElementById("projectDescription").textContent = description;
+    document.getElementById("projectImage").src = "images/portfolio/" + image;
+
+    const projectGithub = document.getElementById("projectGithub");
+    if (github.length <= 0) {
+        projectGithub.href = "";
+        projectGithub.innerHTML =
+            "<i class='fas fa-eye-slash'></i> Source Private";
+    } else {
+        projectGithub.href = github;
+        projectGithub.innerHTML = "<i class='fab fa-github'></i> View Source";
+    }
+}
+
+function selectedProjectUnderline(clickedElem) {
+    for (var i = 0; i < portfolioMenuItems.length; i++) {
+        const elem = portfolioMenuItems[i];
+        elem.classList.remove("selected");
+    }
+    clickedElem.classList.add("selected");
+}
+
+//About section logic
+const aboutBtn = document.getElementById("aboutBtn");
+const aboutContainer = document.getElementById("aboutContainer");
+
+aboutBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    closeAllContainers();
+
+    aboutContainer.style.display = "flex";
+});
+
+//Contact section logic
+const contactBtn = document.getElementById("contactBtn");
+const contactContainer = document.getElementById("contactContainer");
+
+contactBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    closeAllContainers();
+
+    contactContainer.style.display = "flex";
+});
+
+//Site wide logic
+function closeAllContainers() {
+    aboutContainer.style.display = "none";
+    portfolioContainer.style.display = "none";
+    contactContainer.style.display = "none";
+}
